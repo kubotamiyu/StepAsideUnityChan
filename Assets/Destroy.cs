80321c01aa9ee44644b3ef422d607d51529e1c4f
久保田実優 <faxl@outlook.jp>
2018-05-28 17:48:39 +0900

    StepAsideUnityChanプロジェクト
    Lesson6のStepAsideUnityChanを作成したファイルです。

---
+++ Assets/Destroy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour
+{
+    private Renderer renderer;
+    private GameObject maincamera;
+    void Start()
+    {
+        maincamera = GameObject.Find("Main Camera");
+       
+    }
+    void Update()
+    {
+        if (transform.position.z < maincamera.transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+   /* void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }*/
+}
